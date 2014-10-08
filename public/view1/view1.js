4c9c2f3f9aad133ad1099020e31c104b9bcba22a
Jim Lindstrom <jim.lindstrom@gmail.com>
2014-10-07 20:43:26 -0400

    Adding a short explanation of each program.

--- public/view1/view1.js
+++ public/view1/view1.js
@@ -25,24 +25,29 @@ angular.module('myApp.view1', ['ngRoute'])
     {
       id: 0,
       name: "(X - Y) * 3, with Master Programmer",
-      program: TestSubtractConstantsWithMasterProgrammer
+      program: TestSubtractConstantsWithMasterProgrammer,
+      description: "This program uses the Master Programmer to orchestrate the other units. First it tells the constant transmitter to send X to Accumulator 1. Then it tells the constant transmitter to send Y to Accuulator 2. It then tells the Accumulators to subtract the numbers and triple the difference. You should see the result in Accumulator 2 after 13 'Addition Times.'"
     },
     {
       id: 1,
       name: "Add Two Constants",
-      program: TestAddTwoConstantsOnce
+      program: TestAddTwoConstantsOnce,
+      description: "This program uses the constant transmitter and both accumulators to simply add two numbers that are initially stored in the constant transmitter. Rather than relying on the Master Programmer, each operation is responsible for initiating the next operation in the chain of operations."
     },
     {
       id: 2,
       name: "Transfer a Negative Number",
-      program: TestAccToAccNegNumTransfer
+      program: TestAccToAccNegNumTransfer,
+      description: "This is a simple program to test the sending of a negative number across the digit tray (data bus)."
     },
   ];
   $scope.test_program = $scope.test_programs[0].id;
+  $scope.test_program_description = $scope.test_programs[0].description;
 
   $scope.load_program = function () {
     $scope.eniac = new Eniac.build({});
     $scope.test_programs[$scope.test_program].program.setup($scope.eniac);
+    $scope.test_program_description = $scope.test_programs[$scope.test_program].description;
   };
 
   $scope.eniac = new Eniac.build({});
